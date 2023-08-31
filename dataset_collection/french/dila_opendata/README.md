e59fe98e25b907f3b00bc94e932450d4875216da
ManuelFay <manuel.faysse@illuin.tech>
2023-08-31 17:30:30 +0200

    bug fic

--- dataset_collection/french/dila_opendata/README.md
+++ dataset_collection/french/dila_opendata/README.md
@@ -34,9 +34,16 @@ in /AMF, are the Autorité des Marchés Financiers data.
 
 ### Preprocessing
 
-#### LEGI 
-
 To parse XML document and retrieve the text body, run script with:
 ```bash
-python scrapping.py --base_path data/echanges.dila.gouv.fr/OPENDATA/LEGI --save_dir data/ --prefix LEGI --hub_id manu/legifrance
+python scrapping.py --base_path data/echanges.dila.gouv.fr/OPENDATA/LEGI --save_dir data/ --prefix LEGI --hub_id manu/dila_legifrance
+python scrapping.py --base_path data/echanges.dila.gouv.fr/OPENDATA/KALI --save_dir data/ --prefix KALI --hub_id manu/dila_kali
+python scrapping.py --base_path data/echanges.dila.gouv.fr/OPENDATA/Questions-Reponses --save_dir data/ --prefix QR --hub_id manu/dila_questions_reponses
+python scrapping.py --base_path data/echanges.dila.gouv.fr/OPENDATA/CNIL --save_dir data/ --prefix CNIL --hub_id manu/dila_cnil
+```
+
+## SLURM
+
+```bash
+sbatch --job-name=cpu --nodes=1 --time=4:00:00 -p cpu_med --cpus-per-task 16 --error=log.err --output=log.out --wrap="python scrapping.py --base_path data/echanges.dila.gouv.fr/OPENDATA/LEGI --save_dir data/ --prefix LEGI --hub_id manu/legifrance"
 ```
--- dataset_collection/french/dila_opendata/scrapping.py
+++ dataset_collection/french/dila_opendata/scrapping.py
@@ -5,7 +5,6 @@ import xml.etree.ElementTree as ET
 import shutil
 import logging
 from tqdm import tqdm
-import pandas as pd
 import argparse
 
 # Walk all files in a directory using os.walk
@@ -13,16 +12,30 @@ import argparse
 logging.basicConfig(level=logging.WARNING)
 
 
-def extract_all_text_from_node(node):
+def extract_text_from_node(x):
     text = ""
-    for child in node:
-        if child.text:
-            text += child.text
-        text += extract_all_text_from_node(child)
+    if x.text:
+        if x.text.strip() != "":
+            text += x.text + "\n"
+    if x.tail:
+        if x.tail.strip() != "":
+            text += x.tail
     return text
 
 
-def explore_node(path, write_path: str):
+def extract_all_text_from_node(node, whitelist=None):
+    if whitelist and (node.tag in whitelist):
+        return extract_all_text_from_node(node)
+
+    text = ""
+    for child in node:
+        if (child.text or child.tail) and ((whitelist and child.tag in whitelist) or (not whitelist)):
+            text += extract_text_from_node(child)
+        text += extract_all_text_from_node(child, whitelist=whitelist)
+    return text
+
+
+def explore_node(path, write_path: str, mode=None):
     for root, dirs, files in tqdm(os.walk(path), desc=f"Exploring {path}"):
         for file in files:
             # Unzip
@@ -31,9 +44,15 @@ def explore_node(path, write_path: str):
                 # rename as tar.gz
                 if file.endswith(".taz"):
                     logging.info(f"Extracting {os.path.join(root, file)}")
-                    tar = tarfile.open(os.path.join(root, file), "r:gz")
-                    tar.extractall(path=folder_path)
-                    tar.close()
+                    # uncompress from command line
+                    # os.system(f"zcat {os.path.join(root, file)} | tar -xvf -")
+                    # mkdir folder_path
+                    os.system(f"mkdir {folder_path}")
+                    os.system(f"tar -zxvf {os.path.join(root, file)} -C {folder_path}")
+
+                    # tar = tarfile.open(os.path.join(root, file), "r:gz")
+                    # tar.extractall(path=folder_path)
+                    # tar.close()
                 if file.endswith(".tar.gz"):
                     logging.info(f"Extracting {os.path.join(root, file)}")
                     tar = tarfile.open(os.path.join(root, file), "r:gz")
@@ -42,7 +61,7 @@ def explore_node(path, write_path: str):
                     # os.remove(os.path.join(root, file))
 
                 # Depth first exploration
-                explore_node(folder_path, write_path)
+                explore_node(folder_path, write_path, mode=mode)
                 # Delete after extraction
                 shutil.rmtree(folder_path)
 
@@ -56,23 +75,12 @@ def explore_node(path, write_path: str):
                 except:
                     logging.warning(f"Could not parse {os.path.join(root, file)}")
                     continue
-                for child in root_xml:
-                    if child.tag == "CONTEXTE":
-                        parsed_doc["headers"] = extract_all_text_from_node(child)
-                    if child.tag == "BLOC_TEXTUEL":
-                        parsed_doc["body"] = extract_all_text_from_node(child)
-                        parsed_doc["path"] = os.path.join(root, file)
 
-                if "body" not in parsed_doc:
+                parsed_doc = {}
+                parsed_doc["path"] = os.path.join(root, file)
+                parsed_doc["body"] = dataset_specific_parser(root_xml, mode=mode)
+                if parsed_doc["body"].replace("\n", "").strip() == "":
                     parsed_doc = {}
-                elif parsed_doc["body"].replace("\n", "").strip() == "":
-                # check if empty body or \n only
-                    parsed_doc = {}
-
-                # if parsed_doc == {}:
-                #     # Plan B, extract everything
-                #     parsed_doc["all"] = extract_all_text_from_node(root_xml)
-                #
 
                 # Write to JSONL file
                 if parsed_doc != {}:
@@ -82,17 +90,17 @@ def explore_node(path, write_path: str):
                         f.write(json.dumps(parsed_doc) + "\n")
 
 
-# def jsonl_to_pandas(jsonl_path: str):
-#
-#     df = pd.read_json(jsonl_path, lines=True)
-#     # rename columns: headers -> title, body -> text
-#     df = df.rename(columns={"headers": "title", "body": "text"})
-#     # remove duplicates
-#     df = df.drop_duplicates(subset=["text"])
-#     # remove empty texts
-#     df = df[df["text"] != ""]
-#
-#     return df
+def dataset_specific_parser(child, mode):
+    if mode == "KALI":
+        return extract_all_text_from_node(child, whitelist=["CONTENU"])
+    if mode == "QR":
+        return extract_all_text_from_node(child, whitelist=["TEXTE_QUESTION", "TEXTE_REPONSE", "Texte_Reponse", "Texte_Question"])
+    if mode == "LEGI":
+        headers = extract_all_text_from_node(child, whitelist=["CONTEXTE"])
+        return extract_all_text_from_node(child, whitelist=["BLOC_TEXTUEL"])
+    if mode == "CNIL":
+        return extract_all_text_from_node(child, whitelist=["CONTENU"])
+    return extract_all_text_from_node(child)
 
 
 if __name__ == "__main__":
@@ -100,21 +108,22 @@ if __name__ == "__main__":
     parser.add_argument("--base_path", type=str, default="./")
     parser.add_argument("--save_dir", type=str, default="./")
     parser.add_argument("--prefix", type=str, default="dila")
-    parser.add_argument("--hub_id", type=str, default="manu/dila_opendata")
+    parser.add_argument("--hub_id", type=str, default=None)
     args = parser.parse_args()
 
     save_path = os.path.join(args.save_dir, f"{args.prefix}_opendata.jsonl")
     if os.path.exists(save_path):
         os.remove(save_path)
-    explore_node(args.base_path, save_path)
+    print(f"Saving to {save_path}")
+    explore_node(args.base_path, save_path, mode=args.prefix.upper())
 
-    from datasets import load_dataset
-
-    dataset = load_dataset("json", data_files=save_path)
-    dataset = dataset.remove_columns(["path"])
-    dataset = dataset.rename_column("headers", "title")
-    dataset = dataset.rename_column("body", "text")
-    dataset.push_to_hub(args.hub_id)
+    if args.hub_id:
+        from datasets import load_dataset
+        dataset = load_dataset("json", data_files=save_path)
+        dataset = dataset.remove_columns(["path"])
+        dataset = dataset.rename_column("headers", "title")
+        dataset = dataset.rename_column("body", "text")
+        dataset.push_to_hub(args.hub_id)
 
     # logging.info(f"Converting {save_path} to csv")
     # jsonl_to_pandas(save_path).to_csv(os.path.join(args.save_dir, f"{args.prefix}_opendata.csv"))
