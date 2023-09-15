20de11ee7a53513ab3de6e411ced22be3cbe8c7c
ManuelFay <manuel.faysse@illuin.tech>
2023-09-15 19:13:04 +0200

    max steps

--- mock_training_run/run_mlm.sh
+++ mock_training_run/run_mlm.sh
@@ -1,14 +1,16 @@
 python mock_training_run/run_mlm.py \
     --model_name_or_path microsoft/deberta-v3-xsmall \
-    --dataset_name manu/french_open_subtitles \
+    --dataset_name  oscar-corpus/OSCAR-2301 \
+    --dataset_config_name fr \
     --gradient_accumulation_steps 8 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 16 \
     --do_train \
-    --do_eval \
+    --do_eval false \
     --output_dir ./data/test-mlm \
     --report_to tensorboard \
     --logging_steps 100 \
     --save_total_limit 3 \
-    --overwrite_output_dir
-    # --streaming
+    --overwrite_output_dir \
+    --max_steps 100000 \
+    --streaming
