51355c4bd6f82c45408510ee7edffbc7d0d99c57
ManuelFay <manuel.faysse@illuin.tech>
2023-09-15 13:43:10 +0200

    add mock training curve

---
+++ mock_training_run/run.sh
@@ -0,0 +1,12 @@
+python mock_training_run/run_clm.py \
+    --model_name_or_path HuggingFaceH4/tiny-random-LlamaForCausalLM \
+    --dataset_name manu/french_open_subtitles \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
+    --do_train \
+    --do_eval \
+    --output_dir ./data/test-clm \
+    --report_to tensorboard \
+    --logging_steps 100 \
+    --save_total_limit 3 \
+    # --streaming \
