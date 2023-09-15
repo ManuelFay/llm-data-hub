c3e593482757fd9df026dc51b52befdf3641bb59
ManuelFay <manuel.faysse@illuin.tech>
2023-09-15 18:58:56 +0200

    test

--- mock_training_run/run_clm.sh
+++ mock_training_run/run_clm.sh
@@ -1,7 +1,8 @@
 python mock_training_run/run_clm.py \
     --model_name_or_path HuggingFaceH4/tiny-random-LlamaForCausalLM \
     --dataset_name manu/french_open_subtitles \
-    --per_device_train_batch_size 8 \
+    --gradient_accumulation_steps 8 \
+    --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 8 \
     --do_train \
     --do_eval \
--- mock_training_run/run_mlm.sh
+++ mock_training_run/run_mlm.sh
@@ -1,12 +1,14 @@
 python mock_training_run/run_mlm.py \
     --model_name_or_path microsoft/deberta-v3-xsmall \
     --dataset_name manu/french_open_subtitles \
-    --per_device_train_batch_size 8 \
-    --per_device_eval_batch_size 8 \
+    --gradient_accumulation_steps 8 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 16 \
     --do_train \
     --do_eval \
     --output_dir ./data/test-mlm \
     --report_to tensorboard \
     --logging_steps 100 \
     --save_total_limit 3 \
+    --overwrite_output_dir
     # --streaming
