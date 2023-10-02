4d06544d9fd7206e6c30de507842b53d039617a9
ManuelFay <manuel.faysse@illuin.tech>
2023-10-02 05:32:00 +0200

    thresh up test samples

--- dataset_construction/utils.py
+++ dataset_construction/utils.py
@@ -5,4 +5,4 @@ def test_set_conformity(dataset, num_test_samples: Optional[int]) -> int:
     """Make sure the test set is not too big."""
     if num_test_samples:
         return min(num_test_samples, len(dataset) // 2)
-    return max(100, len(dataset) // 100)
+    return min(max(100, len(dataset) // 100), 10000)
