e4f6e0c6705ed76b1cebaa405ab4991be0a24060
ManuelFay <manuel.faysse@illuin.tech>
2023-10-13 09:23:31 +0200

    test

--- dataset_construction/tokenizer_build_script.py
+++ dataset_construction/tokenizer_build_script.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import datasets
 from transformers import PreTrainedTokenizerFast, AutoTokenizer
 from dataset_construction.fit_tokenizer import fit_tokenizer, build_tokenizer, refit_tokenizer
@@ -11,43 +12,49 @@ if __name__ == "__main__":
     parser.add_argument("--build_from_scratch", action="store_true", default=False)
     parser.add_argument("--sample_size", type=int, default=None)
     parser.add_argument("--upload_corpus_id", type=str, default=None)
+    parser.add_argument("--local_save_path", type=str, default="data/tok_all")
     args = parser.parse_args()
 
-    # concatenate datasets loaded from disks
-    ds_fr = datasets.load_from_disk("data/tok_fr")["train"]     # .shuffle()
+    if os.path.exists(args.local_save_path):
+        ds = datasets.load_from_disk(args.local_save_path)
+    else:
+        # concatenate datasets loaded from disks
+        ds_fr = datasets.load_from_disk("data/tok_fr")["train"]     # .shuffle()
 
-    ds_code = datasets.load_from_disk("data/tok_code")["train"]     # .shuffle()
-    # ds_code = ds_code.select(range(len(ds_code)//2)) # half it
+        ds_code = datasets.load_from_disk("data/tok_code")["train"]     # .shuffle()
+        # ds_code = ds_code.select(range(len(ds_code)//2)) # half it
 
-    ds_en = datasets.load_from_disk("data/english_30b")["train"]    # .shuffle()
-    print(len(ds_en), ds_en.data.nbytes//1e9)
-    ds_en = ds_en.select(range(len(ds_fr)//2))
-    print(len(ds_en), ds_en.data.nbytes // 1e9)
+        ds_en = datasets.load_from_disk("data/english_30b")["train"]    # .shuffle()
+        print(len(ds_en), ds_en.data.nbytes//1e9)
+        ds_en = ds_en.select(range(len(ds_fr)//2))
+        print(len(ds_en), ds_en.data.nbytes // 1e9)
 
-    print("French")
-    print(ds_fr)
+        print("French")
+        print(ds_fr)
 
-    print("Code")
-    print(ds_code)
+        print("Code")
+        print(ds_code)
 
-    print("English")
-    print(ds_en)
+        print("English")
+        print(ds_en)
 
-    ds = datasets.concatenate_datasets([ds_code, ds_fr, ds_en]).shuffle()
+        ds = datasets.concatenate_datasets([ds_code, ds_fr, ds_en]).shuffle()
 
-    print(f"Size of Concatenated: {ds.data.nbytes//1e9} GB")
-    print(f"Size of French: {ds_fr.data.nbytes//1e9} GB, ratio of {ds_fr.data.nbytes/ds.data.nbytes}")
-    print(f"Size of Code: {ds_code.data.nbytes//1e9} GB, ratio of {ds_code.data.nbytes/ds.data.nbytes}")
-    print(f"Size of English: {ds_en.data.nbytes//1e9} GB, ratio of {ds_en.data.nbytes/ds.data.nbytes}")
+        print(f"Size of Concatenated: {ds.data.nbytes//1e9} GB")
+        print(f"Size of French: {ds_fr.data.nbytes//1e9} GB, ratio of {ds_fr.data.nbytes/ds.data.nbytes}")
+        print(f"Size of Code: {ds_code.data.nbytes//1e9} GB, ratio of {ds_code.data.nbytes/ds.data.nbytes}")
+        print(f"Size of English: {ds_en.data.nbytes//1e9} GB, ratio of {ds_en.data.nbytes/ds.data.nbytes}")
 
-    # ds.save_to_disk("data/tok_all")
-    if args.upload_corpus_id:
-        for i in range(5):
-            try:
-                ds.push_to_hub(args.upload_corpus_id, max_shard_size="2GB")
-                break
-            except:
-                print("Failed to push to hub")
+        ds.save_to_disk(args.local_save_path)
+
+        # ds.save_to_disk("data/tok_all")
+        if args.upload_corpus_id:
+            for i in range(5):
+                try:
+                    ds.push_to_hub(args.upload_corpus_id, max_shard_size="2GB")
+                    break
+                except:
+                    print("Failed to push to hub")
 
     # small scale tests to begin
     if args.sample_size:
