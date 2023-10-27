c7fa5702c7afb232e65092660b540af675105575
ManuelFay <manuel.faysse@illuin.tech>
2023-10-27 15:58:02 +0200

    update args

--- mock_training_run/run_clm.sh
+++ mock_training_run/run_clm.sh
@@ -1,13 +1,16 @@
 python mock_training_run/run_clm.py \
-    --model_name_or_path HuggingFaceH4/tiny-random-LlamaForCausalLM \
-    --dataset_name manu/french_open_subtitles \
+    --model_name_or_path llama_configs/config.json \
+    --tokenizer_name "manu/tok-fr-en-code" \
+    --dataset_name manu/french-30b \
     --gradient_accumulation_steps 8 \
-    --per_device_train_batch_size 2 \
-    --per_device_eval_batch_size 8 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 16 \
     --do_train \
     --do_eval \
     --output_dir ./data/test-clm \
     --report_to tensorboard \
     --logging_steps 100 \
     --save_total_limit 3 \
+    --push_to_hub \
+    --push_to_hub_model_id "manu/test-clm" \
     # --streaming \
