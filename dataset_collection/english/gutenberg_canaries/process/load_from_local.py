5752f2a8c03a43af167b7fab3c7c5b99f90db863
ManuelFay <manuel.faysse@illuin.tech>
2023-11-15 14:10:48 +0100

    gutenberg canaries

---
+++ dataset_collection/english/gutenberg_canaries/process/load_from_local.py
@@ -0,0 +1,23 @@
+# load zip data as a local hf dataset
+
+from datasets import load_from_disk
+# zipfile to decompress
+# from zipfile import ZipFile
+
+# # unzip the dataset
+# PATH = 'dataset_collection/english/gutenberg_canaries/data/gutenberg_canaries.zip'
+#
+# with ZipFile(PATH, 'r') as zipObj:
+#     zipObj.extractall('dataset_collection/english/gutenberg_canaries/data/')
+
+# load the dataset
+dataset = load_from_disk('dataset_collection/english/gutenberg_canaries/data/gutenberg_canaries')
+
+# print the dataset
+print(dataset)
+# print the first sample
+print(dataset[0])
+
+
+# push the dataset to the hub
+dataset.push_to_hub('manu/gutenberg_canaries', private=True)
