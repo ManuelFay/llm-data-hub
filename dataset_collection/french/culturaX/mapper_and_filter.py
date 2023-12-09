a11277f4897fa63620ba0e98b1d106f3b808a41f
ManuelFay <manuel.faysse@illuin.tech>
2023-12-09 09:36:25 +0100

    filter slimpajama

--- dataset_collection/french/culturaX/mapper_and_filter.py
+++ dataset_collection/french/culturaX/mapper_and_filter.py
@@ -6,10 +6,11 @@ from dataset_collection.abstract_mapper_and_filter import Mapper, Filter
 class CulturaXMapper(Mapper):
     def mapper_fn(self, example) -> Dict:
         """Keep only columns programme_id and transcript and rename as id, text"""
-        raise NotImplementedError
+        # regex transform PI info
+        return {"text": example["text"], "id": example['source'] + "_" + example["url"]}
 
 
 class CulturaXFilter(Filter):
     def filter_fn(self, example) -> bool:
         """Remove binary option trading samples"""
-        return "binary" not in example["url"]
+        return "binary" not in example["id"]
